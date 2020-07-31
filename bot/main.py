57a7c1646f888df01c7b99a17df4fc50cf4181f8
Derevin <prochvoj@fit.cvut.cz>
2020-07-31 22:51:14 +0200

    added playing status

--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,14 @@
 import os
-
+import discord
 from discord.ext import commands
 
 from settings import *
 
 bot = commands.Bot(command_prefix = '+')
+
+@bot.event
+async def on_ready():
+  await bot.change_presence(activity=discord.Game(name="PING DEREVIN IF BROKEN"))
 
 for filename in os.listdir("bot/cogs"):
   if filename.endswith(".py") and filename != '__init__.py':
