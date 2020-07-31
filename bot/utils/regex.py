f293e6ed2b80810fd7a93f7d99cfc378f2586604
Derevin <prochvoj@fit.cvut.cz>
2020-07-31 23:25:00 +0200

    add testtest, ayaya regexes

--- bot/utils/regex.py
+++ bot/utils/regex.py
@@ -1,6 +1,25 @@
 import discord
 from discord.ext import commands
+import re
 
-async def regex_handler(message: discord.Message):
-  pass
-  #await message.channel.send("time to check regex")
+regex_commands = []
+
+async def regex_handler(message: discord.Message):  
+  for trig, resp,flags in regex_commands:    
+    if re.search(trig,message.content,flags) != None:
+      await resp(message)
+
+
+
+
+async def ayaya_resp(message: discord.Message):
+  await message.channel.send("Ayaya!")
+
+com_tuple = ('^ayaya$',ayaya_resp,re.M|re.I)
+regex_commands.append(com_tuple)
+
+async def test_resp(message: discord.Message):
+  await message.channel.send("Ayaaaaaaaaaaaa!")
+
+com_tuple = ('^testtest$',test_resp,re.M|re.I)
+regex_commands.append(com_tuple)
