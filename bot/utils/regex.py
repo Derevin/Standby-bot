274da29017f3fe7be6495511d2dc3e56f447885b
Derevin <prochvoj@fit.cvut.cz>
2020-08-01 02:38:54 +0200

    hide ayaya reactions behind guild if

--- bot/utils/regex.py
+++ bot/utils/regex.py
@@ -14,10 +14,11 @@ async def regex_handler(message: discord.Message):
 
 async def ayaya_resp(message: discord.Message):
   await message.channel.send("Ayaya!")
-  await message.add_reaction(':Ayy:610479153937907733')
-  await message.add_reaction(':Ayy2:470743166207787010')
+  if message.guild.id == 315247766739615745:
+    await message.add_reaction(':Ayy:610479153937907733')
+    await message.add_reaction(':Ayy2:470743166207787010')
 
-regex_commands.append(('^ayaya$',ayaya_resp,re.M|re.I))
+regex_commands.append(('^ayaya\W{0,4}$',ayaya_resp,re.M|re.I))
 
 ############ test regex
 
