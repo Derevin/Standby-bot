ad2a5d747690184048600848685a9431fa4338cd
Derevin <prochvoj@fit.cvut.cz>
2020-08-04 16:05:29 +0200

    add sleep to pingsock

--- bot/utils/regex_vftv.py
+++ bot/utils/regex_vftv.py
@@ -1,11 +1,13 @@
 import discord
 import re
 import random
+import asyncio
 
 regex_vftv_commands = []
 
 
 async def pingsock_resp(message: discord.Message):
+    await asyncio.sleep(5)
     if random.randint(1, 4) == 1:
         await message.channel.send(
             f"Do I see someone who loves being pinged, {message.author.mention}?"
