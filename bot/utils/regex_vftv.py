049ae17070d7b2c38e4ade5e9caa5527bc0fe9f9
Derevin <prochvoj@fit.cvut.cz>
2020-08-04 16:18:20 +0200

    blobwave test version

--- bot/utils/regex_vftv.py
+++ bot/utils/regex_vftv.py
@@ -6,6 +6,9 @@ import asyncio
 regex_vftv_commands = []
 
 
+### pinging people
+
+
 async def pingsock_resp(message: discord.Message):
     await asyncio.sleep(5)
     if random.randint(1, 4) == 1:
@@ -16,4 +19,21 @@ async def pingsock_resp(message: discord.Message):
 
 regex_vftv_commands.append(
     ("^<:Pingsock:421725383172554753>$", pingsock_resp, re.M | re.I)
+)
+
+### waving at people
+
+
+async def wave_resp(message: discord.Message):
+    if random.randomint(0, 99) < 99:
+        await message.add_reaction(":BlobWave:382606234148143115")
+
+
+regex_vftv_commands.append(
+    (
+        """^^(<:BlobWave:382606234148143115> ?(<:BlobCoffee:456004868990173198>)?|
+        <:BlobCoffee:456004868990173198> ?(<:BlobWave:382606234148143115>)?)$$""",
+        pingsock_resp,
+        re.M | re.I,
+    )
 )
