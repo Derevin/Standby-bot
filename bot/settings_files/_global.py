d16d90e0e241ae7ec36690a75b824be5ce8c3876
Derevin <prochvoj@fit.cvut.cz>
2020-08-04 15:29:44 +0200

    make GUILD_ID, BOT_ID int, pingsock command

--- bot/settings_files/_global.py
+++ bot/settings_files/_global.py
@@ -1,5 +1,5 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", False)
-BOT_ID = os.getenv("BOT_ID", False)
-GUILD_ID = os.getenv("GUILD_ID", False)
+BOT_ID = int(os.getenv("BOT_ID", False))
+GUILD_ID = int(os.getenv("GUILD_ID", False))
--- bot/utils/regex_vftv.py
+++ bot/utils/regex_vftv.py
@@ -1,16 +1,16 @@
 import discord
 import re
+import random
 
 regex_vftv_commands = []
 
 
 async def pingsock_resp(message: discord.Message):
-    pass
+    if random.randint(1, 4) == 1:
+        await message.channel.send(
+            f"Do I see someone who loves being pinged, {message.author.mention}?"
+        )
 
-
-regex_vftv_commands.append(
-    ("^<:Pingsock:421725383172554753>$", pingsock_resp, re.M | re.I)
-)
 
 regex_vftv_commands.append(
     ("^<:Pingsock:421725383172554753>$", pingsock_resp, re.M | re.I)
