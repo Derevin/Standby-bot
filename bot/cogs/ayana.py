35eac6f0aa58ff88e48a2c521b3fc30a82757b0e
Derevin <prochvoj@fit.cvut.cz>
2020-08-13 16:34:03 +0200

    say something on member leaving

--- bot/cogs/ayana.py
+++ bot/cogs/ayana.py
@@ -1,10 +1,20 @@
 from discord.ext import commands
 import discord
+from settings import *
 
 
 class Ayana(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member: discord.Member):
+        if member.guild.id == GUILD_ID:
+            channel = discord.utils.get(
+                ctx.guild.text_channels, name=ERROR_CHANNEL_NAME
+            )
+            if channel is not None:
+                await channel.send(f"{member} has been noscoped succesfully.")
 
 
 def setup(bot):
