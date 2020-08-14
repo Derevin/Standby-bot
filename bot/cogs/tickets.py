18e7d78a1a39022ac1343e1f09a2099be1918ab6
Derevin <prochvoj@fit.cvut.cz>
2020-08-14 20:36:51 +0200

    updated channel name in log

--- bot/cogs/tickets.py
+++ bot/cogs/tickets.py
@@ -159,7 +159,7 @@ class Tickets(commands.Cog):
         embed.set_thumbnail(url=message.author.avatar_url)
         embed.title = message.author.name
         embed.description = f"[{content_msg}]({message.jump_url})"
-        embed.add_field(name="Channel", value=message.channel.mention)
+        embed.add_field(name="Channel", value=message.channel.name)
         embed.add_field(name="Date", value=message.created_at)
         return embed
 
