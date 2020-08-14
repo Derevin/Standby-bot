0efb7944f1785d9ec955983b2dd18a93a3060612
Derevin <prochvoj@fit.cvut.cz>
2020-08-14 17:46:23 +0200

    tclaim implemented minus permissions

--- bot/cogs/tickets.py
+++ bot/cogs/tickets.py
@@ -10,11 +10,16 @@ ACTIVE_TICKETS_CAT_NAME = "Active tickets"
 RESOLVED_TICKETS_CAT_NAME = "Resolved tickets"
 CLAIMABLE_CHANNEL_MESSAGE = (
     "If you want to talk to the mod team, this is the place!\n"
-    "Claim this channel by typing ```+topen```"
-    " and then this channel will be restricted for your and mod-team eyes only\n"
+    "Claim this channel by typing: ```+topen```"
+    " and then this channel will be restricted for your and mod-team eyes only.\n"
     "Disclamer: It is recommended to mute this channel's category,"
     " otherwise you will get an unread message notification "
-    "everytime this channel get recreated"
+    "everytime this channel gets recreated."
+)
+CLAIMED_MESSAGE = (
+    "This channel has been claimed,"
+    " you can make sure you're talking only to the mod team by looking "
+    "at current member list of the channel (right side of discord)."
 )
 
 
@@ -28,10 +33,7 @@ class Tickets(commands.Cog):
         )
         await chnl.send(CLAIMABLE_CHANNEL_MESSAGE)
 
-    @commands.command()
-    @commands.has_any_role("Moderator", "Guides of the Void")
-    async def tinit(self, ctx, *args):
-        print(ctx.guild.categories)
+    async def get_or_create_open_cat(self, ctx):
         open_ticket_cat = discord.utils.get(
             ctx.guild.categories, name=CLAIMABLE_TICKETS_CAT_NAME
         )
@@ -40,7 +42,51 @@ class Tickets(commands.Cog):
                 name=CLAIMABLE_TICKETS_CAT_NAME,
                 reason="Making a category for claimable tickets.",
             )
-        print(open_ticket_cat.channels)
+        return open_ticket_cat
+
+    async def get_or_create_active_cat(self, ctx):
+        active_ticket_cat = discord.utils.get(
+            ctx.guild.categories, name=ACTIVE_TICKETS_CAT_NAME
+        )
+        if active_ticket_cat is None:
+            active_ticket_cat = await ctx.guild.create_category(
+                name=ACTIVE_TICKETS_CAT_NAME,
+                reason="Making a category for claimable tickets.",
+            )
+        return active_ticket_cat
+
+    async def get_or_create_resolved_cat(self, ctx):
+        resolved_ticket_cat = discord.utils.get(
+            ctx.guild.categories, name=RESOLVED_TICKETS_CAT_NAME
+        )
+        if resolved_ticket_cat is None:
+            resolved_ticket_cat = await ctx.guild.create_category(
+                name=RESOLVED_TICKETS_CAT_NAME,
+                reason="Making a category for claimable tickets.",
+            )
+        return resolved_ticket_cat
+
+    async def get_highest_num(self, ctx):
+        active_ticket_cat = await self.get_or_create_active_cat(ctx)
+        resolved_ticket_cat = await self.get_or_create_resolved_cat(ctx)
+
+        num = 0
+        for x in active_ticket_cat.channels:
+            lst = x.name.split("-")
+            if int(lst[-1]) > num:
+                num = int(lst[-1])
+
+        for x in resolved_ticket_cat.channels:
+            lst = x.name.split("-")
+            if int(lst[-1]) > num:
+                num = int(lst[-1])
+
+        return num
+
+    @commands.command()
+    @commands.has_any_role("Moderator", "Guides of the Void")
+    async def tinit(self, ctx, *args):
+        open_ticket_cat = await self.get_or_create_open_cat(ctx)
         if not open_ticket_cat.channels:
             await self.create_claimable(open_ticket_cat)
 
@@ -50,13 +96,22 @@ class Tickets(commands.Cog):
             raise commands.errors.UserInputError(
                 "This command can be used only in a claimable channel"
             )
-        print("open")
-        pass
+        issue_num = await self.get_highest_num(ctx) + 1
+        print(issue_num)
+
+        active_ticket_cat = await self.get_or_create_active_cat(ctx)
+        await ctx.channel.edit(
+            name=f"{ctx.author.name}-{issue_num}", category=active_ticket_cat
+        )
+        await ctx.channel.send(CLAIMED_MESSAGE)
+
+        open_ticket_cat = await self.get_or_create_open_cat(ctx)
+        if not open_ticket_cat.channels:
+            await self.create_claimable(open_ticket_cat)
 
     @commands.command()
     async def treopen(self, ctx, *args):
         print("reopen")
-        pass
 
     @commands.command()
     async def tresolve(self, ctx, *args):
