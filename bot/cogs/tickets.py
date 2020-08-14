0a58498fd0fe5979eecf6151749df6fd8b959717
Derevin <prochvoj@fit.cvut.cz>
2020-08-14 20:12:33 +0200

    tickets added message deletion in claimable

--- bot/cogs/tickets.py
+++ bot/cogs/tickets.py
@@ -39,6 +39,32 @@ class Tickets(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+        if (
+            str(message.channel.type) == "text"
+            and str(message.channel.name) == CLAIMABLE_CHANNEL_NAME
+        ):
+            try:
+                if (
+                    str(message.content) != "+tclaim"
+                    and str(message.content) != "+topen"
+                ):
+                    await message.delete()
+            except Exception as e:
+                if message.guild.id == GUILD_ID:
+                    channel = discord.utils.get(
+                        message.guild.text_channels, name=ERROR_CHANNEL_NAME
+                    )
+                    if channel is not None:
+                        await channel.send(
+                            embed=unhandled_error_embed(
+                                message.content, message.channel, e
+                            )
+                        )
+
     @commands.command()
     @commands.has_any_role("Moderator", "Guides of the Void")
     async def tinit(self, ctx, *args):
