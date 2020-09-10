676e596148b52bd120a01dfd9d36ed02b571b084
Feldraas <feldraas@users.noreply.github.com>
2020-09-10 11:23:06 +0200

    Fix for 'dont @ me' resp

--- bot/utils/regex_uncategorized.py
+++ bot/utils/regex_uncategorized.py
@@ -228,7 +228,7 @@ async def dont_at_me_resp(message: discord.Message):
 
 
 regex_uncategorized_commands.append(
-    ("do[ ]?n[o ']?t @ me", dont_at_me_resp, re.M | re.I)
+    ("do[ ]?n[o ']?t (@|at) me", dont_at_me_resp, re.M | re.I)
 )
 
 
