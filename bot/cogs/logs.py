30f87322f19ed8474402c0630d5d8a7c8aa4d046
Feldraas <feldraas@users.noreply.github.com>
2020-09-29 22:27:49 +0200

    Adding mod logs trial

---
+++ bot/cogs/logs.py
@@ -0,0 +1,151 @@
+from discord.ext import commands
+import discord
+from settings import *
+from utils.util_functions import *
+import datetime
+
+
+class Logs(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_raw_message_delete(self, payload):
+        channel = self.bot.get_channel(payload.channel_id)
+        embed = await deleted_embed(payload, channel)
+        if embed:
+            guild = channel.guild
+            logs = discord.utils.get(guild.text_channels, name=LOGS_CHANNEL_NAME)
+            await logs.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_raw_message_edit(self, payload):
+
+        embed = await edited_embed(self.bot, payload)
+        if embed:
+            channel = self.bot.get_channel(payload.channel_id)
+            guild = channel.guild
+            logs = discord.utils.get(guild.text_channels, name=LOGS_CHANNEL_NAME)
+            await logs.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+
+        if before.channel == after.channel:
+            return
+
+        embed = await voice_embed(member, before.channel, after.channel)
+
+        guild = member.guild
+        logs = discord.utils.get(guild.text_channels, name=LOGS_CHANNEL_NAME)
+
+        await logs.send(embed=embed)
+
+
+async def deleted_embed(payload, channel):
+    embed = discord.Embed(color=SOFT_RED)
+    embed.title = "Message deleted"
+    if payload.cached_message is not None:
+        message = payload.cached_message
+        if str(message.type) == "MessageType.pins_add":
+            return None
+        embed.description = message.content
+        embed.set_thumbnail(url=message.author.avatar_url)
+        embed.add_field(name="Author", value=message.author.mention)
+        embed.add_field(name="Channel", value=message.channel.mention)
+        if message.attachments:
+            embed.add_field(
+                name="Attachment",
+                value=f"[Click here]({message.attachments[0].url})",
+                inline=False,
+            )
+    else:
+        embed.description = "[Message not found in cache]"
+        embed.add_field(name="Channel", value=channel.mention)
+    embed.timestamp = datetime.datetime.utcnow()
+    return embed
+
+
+async def edited_embed(bot, payload):
+
+    before = payload.cached_message
+    after = payload.data
+    if "content" in after:
+        after_message = after["content"]
+    else:
+        return None
+    attachment_url = None
+
+    if before:
+
+        author = before.author
+        if author.bot:
+            return None
+
+        before_message = before.content
+        if before_message == after_message:
+            return None
+
+        channel = before.channel
+        jump_url = before.jump_url
+        avatar_url = before.author.avatar_url
+        attachment_url = before.attachments[0].url if before.attachments else None
+
+    else:
+
+        before_message = "[Message not found in cache]"
+
+        guild_id = after["guild_id"]
+        author_id = after["author"]["id"]
+        channel_id = after["channel_id"]
+        message_id = after["id"]
+
+        guild = await bot.fetch_guild(guild_id)
+        author = await guild.fetch_member(author_id)
+        if author.bot:
+            return None
+        channel = await bot.fetch_channel(channel_id)
+        message = await channel.fetch_message(message_id)
+        jump_url = message.jump_url
+        avatar_url = author.avatar_url
+        attachment_url = message.attachments[0].url if message.attachments else None
+
+    embed = discord.Embed(color=PALE_YELLOW)
+    embed.title = "Message edited"
+    embed.add_field(name="Before", value=before_message, inline=False)
+    embed.add_field(name="After", value=after_message, inline=False)
+    embed.add_field(name="Author", value=author.mention)
+    embed.add_field(name="Channel", value=channel.mention)
+    embed.add_field(name="Link to Message", value=f"[Click here]({jump_url})")
+    embed.set_thumbnail(url=avatar_url)
+    if attachment_url:
+        embed.add_field(name="Attachment", value=f"[Click here]({attachment_url})")
+    embed.timestamp = datetime.datetime.utcnow()
+    return embed
+
+
+async def voice_embed(member, before, after):
+    embed = discord.Embed(color=PALE_BLUE)
+    embed.title = "Voice channel update"
+
+    if before and after:
+        embed.description = (
+            f"{member.mention} ({member.name}#{member.discriminator}) switched"
+            f" voice channels from **#{before.name}** to **#{after.name}**"
+        )
+    elif before:
+        embed.description = (
+            f"{member.mention} ({member.name}#{member.discriminator}) left"
+            f" voice channel **#{before.name}**"
+        )
+    else:
+        embed.description = (
+            f"{member.mention} ({member.name}#{member.discriminator}) joined"
+            f" voice channel **#{after.name}**"
+        )
+    embed.timestamp = datetime.datetime.utcnow()
+    return embed
+
+
+def setup(bot):
+    bot.add_cog(Logs(bot))
