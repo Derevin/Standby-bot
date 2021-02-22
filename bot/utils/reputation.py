fba85ece080f479c0ab48927eb4d9280fb358e37
Feldraas <feldraas@users.noreply.github.com>
2021-02-22 19:54:18 +0100

    Added debug prints

--- bot/utils/reputation.py
+++ bot/utils/reputation.py
@@ -9,15 +9,23 @@ THANK_TYPE = "Void"
 
 async def reputation_resp(bot, message: discord.Message):
     if message and message.mentions:
+        print("debug: message has thanks + mention")
         for x in message.mentions:
+            print("debug: starting for loop"
             if message.author.id == x.id:
+                print("debug: message is a self-thank")
                 await message.channel.send("Thanking yourself gives no reputation.")
+                print("debug: self-thank message sent")
                 continue
+            print("debug: not a self-thank")
             await ensure_usr_existence(bot, x.id, x.guild.id)
+            print("debug: existence ensured")
             await bot.pg_pool.execute(
                 f"UPDATE usr SET thanks = thanks + 1 WHERE usr_id = {x.id}"
             )
+            print("debug: DB command executed")
             await message.channel.send(f"Gave +1 {THANK_TYPE} to {x.mention}")
+            print("debug: confirmation message sent")
     pass
 
 
