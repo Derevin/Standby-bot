e360d342df4184b497067504014f379580354d63
Derevin <thederevin@gmail.com>
2021-07-29 23:24:11 +0200

    hide reason finding behind try-catch

--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,7 @@ import subprocess
 import discord
 import asyncio
 import re
+import traceback
 from discord.ext import commands
 from settings import *
 
@@ -20,17 +21,25 @@ async def on_ready():
     await bot.change_presence(activity=discord.Game(name="Have a nice day!"))
     channel = bot.get_channel(ERROR_CHANNEL_ID)
     if channel:
-        # logs = str(subprocess.check_output("heroku logs -n 15", shell=True))
-        # chunks = re.split("\\\\n", logs)
-        reason_found = "unkown"
-        # next_is_reason = False
-        # for c in reversed(chunks):
-        #     if next_is_reason:
-        #         reason_found = c.split("]: ")[1]
-        #         break
-        #     if "State changed from up to" in c:
-        #         next_is_reason = True
-        #         continue
+        reason_found = "not found"
+        try:
+            logs = str(
+                subprocess.check_output(
+                    "heroku logs -n 15 --app=standby-bot", shell=True
+                )
+            )
+            chunks = re.split("\\\\n", logs)
+            next_is_reason = False
+            for c in reversed(chunks):
+                if next_is_reason:
+                    reason_found = c.split("]: ")[1]
+                    break
+                if "State changed from up to" in c:
+                    next_is_reason = True
+                    continue
+        except Exception:
+            reason_found = "uknown"
+            traceback.print_exc()
         await channel.send(f"Reboot complete. Reason:{reason_found}")
 
 
