6ae6f31b56ab176bf66aad0375fd73cc23194c20
Feldraas <feldraas@users.noreply.github.com>
2022-03-08 15:36:41 +0100

    +meme now self-deletes by default, +memed removed

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -196,12 +196,10 @@ class Fun(commands.Cog):
         rip = await ctx.channel.send(embed=embed)
         await rip.add_reaction("🇫")
 
-    @commands.command(brief="Posts a meme", aliases=["memed"], help=help_text)
+    @commands.command(brief="Posts a meme", help=help_text)
     async def meme(self, ctx, *, query):
 
-        cmd = re.split(" ", ctx.message.content)[0][1:]
-        if cmd == "memed":
-            await ctx.message.delete()
+        await ctx.message.delete()
 
         matches = process.extractOne(
             query, list(memes.keys()), scorer=fuzz.token_set_ratio, score_cutoff=67
