7153ac4cb05b05dbc1b501f92968c5471c97f46b
Feldraas <feldraas@users.noreply.github.com>
2022-03-22 18:40:02 +0100

    Re-enabled image previews for WF mods

--- bot/settings_files/warframe_data.py
+++ bot/settings_files/warframe_data.py
@@ -3,10 +3,11 @@ import requests
 import re
 
 
-MODS_URL = "https://raw.githubusercontent.com/WFCD/warframe-items/development/data/json/Mods.json"
+MODS_URL = (
+    "https://raw.githubusercontent.com/WFCD/warframe-items/master/data/json/Mods.json"
+)
 r = requests.get(url=MODS_URL)
-# mod_data = r.json()
-mod_data = {}
+mod_data = r.json()
 
 mod_list = {}
 
@@ -14,9 +15,3 @@ for mod in mod_data:
 
     if "wikiaThumbnail" in mod:
         mod_list[mod["name"].lower()] = mod["wikiaThumbnail"]
-
-    elif mod["name"] == "Cunning Drift":
-        mod_list[
-            mod["name"].lower()
-        ] = "https://vignette.wikia.nocookie.net/warframe/images/9/9c/Cunning_drift.png/revision/latest"
-
