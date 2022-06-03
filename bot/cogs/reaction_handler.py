c727b45ac713e02c4007f90bff22f02b5813782f
Feldraas <feldraas@users.noreply.github.com>
2022-06-03 23:01:00 +0200

    Ignoring voice channels until library update

--- bot/cogs/reaction_handler.py
+++ bot/cogs/reaction_handler.py
@@ -4,13 +4,26 @@ from utils.starboard import starboard_handler
 from cogs.giveaways import giveaway_handler
 from cogs.rules import role_handler
 from cogs.services import urban_handler
+import functools
 
 
 class ReactionHandler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def ignore_in_voice_channel(func):
+        @functools.wraps(func)
+        async def wrapper(*args):
+            bot = args[0].bot
+            payload = args[1]
+            channel = bot.get_channel(payload.channel_id)
+            if not (channel and str(channel.type) == "voice"):
+                await func(*args)
+
+        return wrapper
+
     @commands.Cog.listener()
+    @ignore_in_voice_channel
     async def on_raw_reaction_add(self, payload):
         await starboard_handler(self.bot, payload)
         await giveaway_handler(self.bot, payload)
@@ -18,15 +31,18 @@ class ReactionHandler(commands.Cog):
         await urban_handler(self.bot, payload)
 
     @commands.Cog.listener()
+    @ignore_in_voice_channel
     async def on_raw_reaction_remove(self, payload):
         await starboard_handler(self.bot, payload)
         await role_handler(self.bot, payload)
 
     @commands.Cog.listener()
+    @ignore_in_voice_channel
     async def on_raw_reaction_clear(self, payload):
         await starboard_handler(self.bot, payload)
 
     @commands.Cog.listener()
+    @ignore_in_voice_channel
     async def on_raw_reaction_clear_emoji(self, payload):
         await starboard_handler(self.bot, payload)
 
