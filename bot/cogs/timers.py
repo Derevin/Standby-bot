33edb5ede0868ccf24bf1e4c79b1600d8d78904d
Feldraas <feldraas@gmail.com>
2022-06-27 19:43:30 +0200

    Added exact time functionality for /remindme

--- bot/cogs/timers.py
+++ bot/cogs/timers.py
@@ -56,13 +56,17 @@ class Timers(commands.Cog):
             traceback.print_exc(file=sys.stdout)
             return
 
-    @nextcord.slash_command(description="Reminds you after a specified time")
-    async def remindme(
+    @nextcord.slash_command()
+    async def remindme():
+        pass
+
+    @remindme.subcommand(description="Reminds you after a specified time", name="in")
+    async def remindme_in(
         self,
         interaction: Interaction,
-        days: int = SlashOption(description="Days until the giveaway finishes"),
-        hours: int = SlashOption(description="Hours until the giveaway finishes"),
-        minutes: int = SlashOption(description="Minutes until the giveaway finishes"),
+        days: int = SlashOption(description="Days until the reminder", min_value=0),
+        hours: int = SlashOption(description="Hours until the reminder", min_value=0),
+        minutes: int = SlashOption(description="Minutes until the reminder", min_value=0),  # fmt: skip
         message=SlashOption(description="A message for the reminder"),
     ):
 
@@ -81,23 +85,67 @@ class Timers(commands.Cog):
         tfuture = timenow + tdelta
         tfuture = tfuture.replace(microsecond=0)
 
-        await ensure_guild_existence(self.bot, interaction.guild.id)
-        await get_or_insert_usr(self.bot, interaction.user.id, interaction.guild.id)
+        await create_reminder(self.bot, interaction, tfuture, message)
 
-        params_dict = {"msg": message, "channel": interaction.channel.id}
-        params_json = json.dumps(params_dict)
-
-        await self.bot.pg_pool.execute(
-            "INSERT INTO tmers (usr_id, expires, ttype, params) VALUES ($1, $2, $3, $4);",
-            interaction.user.id,
-            tfuture,
-            DB_TMER_REMINDER,
-            params_json,
-        )
         await interaction.send(
             f"{dynamic_timestamp(timenow, 'short')}: Your reminder has been registered "
             f"and you will be reminded on {dynamic_timestamp(tfuture, 'long')}."
         )
+
+    @remindme.subcommand(
+        description="Reminds you at a specified date and time", name="at"
+    )
+    async def remindme_at(
+        self,
+        interaction: Interaction,
+        year: int = SlashOption(description="Year of the reminder"),
+        month: int = SlashOption(description="Month of the reminder"),
+        day: int = SlashOption(description="Day of the reminder"),
+        hour: int = SlashOption(description="Hour of the reminder"),
+        minute: int = SlashOption(description="Minute of the reminder"),
+        message: str = SlashOption(description="A message for the reminder"),
+    ):
+        timenow = datetime.now()
+        try:
+            tfuture = datetime(
+                year=year, month=month, day=day, hour=hour, minute=minute
+            )
+        except ValueError:
+            await interaction.send(
+                "Please input a valid date and time.", ephemeral=True
+            )
+            return
+
+        if tfuture < timenow:
+            await interaction.send(
+                f"You must choose a time that's in the future (current bot time is {timenow.strftime('%H:%M')}).",
+                ephemeral=True,
+            )
+            return
+
+        await create_reminder(self.bot, interaction, tfuture, message)
+
+        await interaction.send(
+            f"{dynamic_timestamp(timenow, 'short')}: Your reminder has been registered "
+            f"and you will be reminded on {dynamic_timestamp(tfuture, 'long')}."
+        )
+
+
+async def create_reminder(bot, interaction, tfuture, message):
+
+    await ensure_guild_existence(bot, interaction.guild.id)
+    await get_or_insert_usr(bot, interaction.user.id, interaction.guild.id)
+
+    params_dict = {"msg": message, "channel": interaction.channel.id}
+    params_json = json.dumps(params_dict)
+
+    await bot.pg_pool.execute(
+        "INSERT INTO tmers (usr_id, expires, ttype, params) VALUES ($1, $2, $3, $4);",
+        interaction.user.id,
+        tfuture,
+        DB_TMER_REMINDER,
+        params_json,
+    )
 
 
 def setup(bot):
