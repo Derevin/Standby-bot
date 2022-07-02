3cc7dfb5c5f6d3779757dd7e65d0e786e2c39cf3
Feldraas <feldraas@gmail.com>
2022-07-02 22:20:00 +0200

    Ticket functionality reworked

--- bot/cogs/tickets.py
+++ bot/cogs/tickets.py
@@ -7,33 +7,30 @@ from inspect import Parameter
 from utils.util_functions import *
 
 CLAIMABLE_TICKETS_CAT_NAME = "Talk to mods"
-CLAIMABLE_CHANNEL_NAME = "claim-this-channel"
+CLAIMABLE_CHANNEL_NAME = "ticket-channel"
 ACTIVE_TICKETS_CAT_NAME = "Active tickets"
 RESOLVED_TICKETS_CAT_NAME = "Resolved tickets"
 TICKETS_LOG_CHANNEL_NAME = "tickets-log"
 CLAIMABLE_CHANNEL_MESSAGE = (
-    "If you have an issue and want to talk to the mod team, this is the place!\n"
-    "Claim this channel by typing: ```/ticket claim```"
-    " and then this channel will be restricted for your and mod-team eyes only.\n"
-    "Disclaimer: It is recommended to mute this channel's category,"
-    " otherwise you will get an unread message notification "
-    "everytime this channel gets recreated."
+    "If you have an issue and want to talk to the mod team, this is the place.\n"
+    "Use the `/ticket` command and choose the `Create` option to open a ticket.\n"
+    "This will open a private channel visible only to you and the mod team."
 )
 CLAIMED_MESSAGE = (
-    "You have successfully claimed this channel, please type what is it that you want to discuss.\n"
+    "You have successfully opened a ticket - please let us know what you want to discuss.\n"
     "You can make sure you're talking only to the mod team by looking "
-    "at current member list of the channel (right side of discord).\n"
-    "Once this issue has been resolved, type: ```/ticket resolve```"
+    "at the channel's current member list (right side of discord).\n"
+    "Once this issue has been resolved, use the `/ticket` command and choose the `Resolve` option."
 )
 
 RESOLVED_MESSAGE = (
-    "This issue has been marked as resolved."
-    " If this was a mistake, type: ```/ticket reopen``` otherwise open a new issue.\n"
-    "This issue can be scrapped with ```+tscrap``` by moderators.\n"
-    "Scrapping takes a while to complete."
+    "This ticket has been marked as resolved."
+    " If this was a mistake or you have additional questions, use the `/ticket` command and choose the `Reopen` option\n"
+    "For other issues, please create a new ticket in XXX.\n"
+    "Moderators can use the `/scrap` command to scrap this ticket. (Scrapping takes a while to complete)"
 )
 
-REOPENED_MESSAGE = "This issue has been reopened. Once the issue is resolved, type: ```+ticket resolve```"
+REOPENED_MESSAGE = "This ticket has been reopened. Once it is resolved, use the`/ticket` command and choose the `Resolve` option again."
 
 
 class Tickets(commands.Cog):
@@ -59,13 +56,13 @@ class Tickets(commands.Cog):
                         embed=unhandled_error_embed(message.content, message.channel, e)
                     )
 
-    async def claim(self, interaction):
+    async def create(self, interaction):
 
         claimable_channel = get_channel(interaction.guild, CLAIMABLE_CHANNEL_NAME)
         if interaction.channel != claimable_channel:
 
             await interaction.send(
-                f"This command can be used only in {claimable_channel.mention}.",
+                f"This command can only be used in {claimable_channel.mention}.",
                 ephemeral=True,
             )
             return
@@ -95,24 +92,26 @@ class Tickets(commands.Cog):
             f"You can now head over to {ticket_chnl.mention}.", ephemeral=True
         )
 
-    # @ticket.subcommand(description="Marks your ticket as resolved")
     async def resolve(self, interaction):
         if interaction.channel.category.name != ACTIVE_TICKETS_CAT_NAME:
             await interaction.send(
-                "This command can be used only in an active channel", ephemeral=True
+                "This command can only be used in an active ticket channel",
+                ephemeral=True,
             )
             return
 
         resolved_ticket_cat = await self.get_or_create_resolved_cat(interaction)
         await interaction.channel.edit(category=resolved_ticket_cat)
 
-        await interaction.send(RESOLVED_MESSAGE)
+        claimable_channel = get_channel(interaction.guild, CLAIMABLE_CHANNEL_NAME)
+        await interaction.send(
+            RESOLVED_MESSAGE.replace("XXX", claimable_channel.mention)
+        )
 
-    # @ticket.subcommand(description="Reopens a resolved ticket")
     async def reopen(self, interaction):
         if interaction.channel.category.name != RESOLVED_TICKETS_CAT_NAME:
             await interaction.send(
-                "This command can be used only in a resolved channel",
+                "This command can only be used in a resolved channel",
                 ephemeral=True,
             )
             return
@@ -123,33 +122,30 @@ class Tickets(commands.Cog):
         await interaction.send(REOPENED_MESSAGE)
 
     @nextcord.slash_command(
-        description="Create a ticket to discuss matters privately with the mod team",
+        description="Create or manage tickets to discuss matters privately with the mod team",
     )
     async def ticket(
         self,
         interaction,
         action=SlashOption(
             description="Choose the action you want to take",
-            choices={
-                "Claim the ticket channel": "claim",
-                "Resolve the current ticket": "resolve",
-                "Reopen a resolved ticket": "reopen",
-            },
+            choices=["Create", "Resolve", "Reopen"],
         ),
     ):
         args_dict = {
-            "claim": self.claim,
-            "resolve": self.resolve,
-            "reopen": self.reopen,
+            "Create": self.create,
+            "Resolve": self.resolve,
+            "Reopen": self.reopen,
         }
         command = args_dict[action]
         await command(interaction)
 
-    @commands.command(
-        brief="Initiates ticket system - creates categories, channels etc"
+    @nextcord.slash_command(
+        description="Initiates ticket system - creates categories, channels etc",
+        default_member_permissions=MODS_AND_GUIDES,
     )
-    @commands.has_any_role(*MOD_ROLES)
-    async def tinit(self, interaction, *args):
+    #
+    async def initiate_ticket_system(self, interaction):
         claimable_ticket_cat = await self.get_or_create_claimable_cat(interaction)
         if not claimable_ticket_cat.channels:
             await self.create_claimable_channel(claimable_ticket_cat)
@@ -157,17 +153,18 @@ class Tickets(commands.Cog):
         await self.get_or_create_resolved_cat(interaction)
         await self.get_or_create_tickets_log(interaction)
 
-    @commands.command(
-        brief="Scraps a resolved ticket, logs the messages and deletes the channel"
+    @nextcord.slash_command(
+        description="Scraps a resolved ticket, logs the messages and deletes the channel",
+        default_member_permissions=MODS_AND_GUIDES,
     )
-    @commands.has_any_role(*MOD_ROLES)
-    async def tscrap(self, interaction):
+    async def scrap(self, interaction):
         if interaction.channel.category.name != RESOLVED_TICKETS_CAT_NAME:
             await interaction.send(
-                "This command can be used only in a resolved channel", ephemeral=True
+                "This command can only be used in a resolved channel", ephemeral=True
             )
             return
 
+        await interaction.send("Scrapping in progress", ephemeral=True)
         tickets_log = await self.get_or_create_tickets_log(interaction)
         msg_list = await interaction.channel.history(
             limit=500, oldest_first=True
